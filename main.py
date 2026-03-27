d8e4902dd0763249a33fb3c6294409f4d2f9228c
Andy Armstrong <andy@hexten.net>
2026-03-27 19:35:54 +0000

    Write to ext drive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import numpy as np
 
@@ -12,7 +14,8 @@ def erode(img: np.ndarray, kernel_size: int = 3, iterations: int = 1) -> np.ndar
     return cv2.erode(img, kernel, iterations=iterations)
 
 
-def main(src: str):
+def main(src: str, out: str):
+    os.makedirs(out, exist_ok=True)
     cap = cv2.VideoCapture(src)
 
     fgbg = cv2.createBackgroundSubtractorMOG2(
@@ -32,12 +35,13 @@ def main(src: str):
         fgmask = fgbg.apply(frame)
         mask1 = dilate(fgmask, kernel_size=3, iterations=1)
         mask2 = erode(mask1, kernel_size=5, iterations=1)
-        mask3 = dilate(mask2, kernel_size=5, iterations=1)
-        mask4 = cv2.GaussianBlur(mask3, (3, 3), 0)
+        mask3 = dilate(mask2, kernel_size=2, iterations=1)
+        mask4 = cv2.GaussianBlur(mask3, (5, 5), 0)
         rgba[:, :, 3] = mask4
+        out_name = os.path.join(out, f"frame{img_num:06d}.png")
         if img_num >= 0:
-            cv2.imwrite(f"tmp/frame{img_num:06d}.png", rgba)
-            print(f"Saved frame{img_num:06d}.png")
+            cv2.imwrite(out_name, rgba)
+            print(f"Saved {out_name}")
         else:
             print(f"Skipping {img_num}")
         img_num += 1
@@ -52,4 +56,4 @@ def main(src: str):
 
 
 if __name__ == "__main__":
-    main("media/PC000003.mov")
+    main("media/PC000003.mov", "/Volumes/Bloater/tmp")
